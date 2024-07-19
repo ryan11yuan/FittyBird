35eaba57ec1fa2f71751b6fb34b900cc20ab7150
Ryan Yuan <105021269+ryan11yuan@users.noreply.github.com>
2024-07-19 00:22:22 -0400

    Delete Flappy Bird App directory

--- Flappy Bird App/js/score.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const finalScore = localStorage.getItem('finalScore');
-    document.getElementById('finalScore').innerText = `Score: ${finalScore}`;
-});
